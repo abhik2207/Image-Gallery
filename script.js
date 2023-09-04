57a4aaf20785b2041532e3dc6d7a375f13990035
Abhik Gupta <107669163+abhik2207@users.noreply.github.com>
2023-09-04 21:36:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let activeIndex = 0;
+const cardGroups = document.getElementsByClassName("card-group");
+
+const handleNextClick = () => {
+    const nextIndex = activeIndex + 1 <= cardGroups.length - 1 ? activeIndex + 1 : 0;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`), nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentGroup.dataset.status = "after";
+    nextGroup.dataset.status = "becoming-active-from-before";
+    setTimeout(()=>{
+        nextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
+
+const handlePrevClick = () => {
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : cardGroups.length - 1;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`), nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentGroup.dataset.status = "before";
+    nextGroup.dataset.status = "becoming-active-from-after";
+    setTimeout(()=>{
+        nextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
